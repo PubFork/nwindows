0c7aa186b7ce10628d10e17322282b466137c032
Jacob DeFilippis <jacobdefilippis@gmail.com>
2016-01-18 14:52:24 -0500

    cleaning nwindow, changes to use assert

--- nwindow.cpp
+++ nwindow.cpp
@@ -2,30 +2,26 @@
 #include <assert.h>
 
 
-
 Nwindow::Nwindow(){
 	/*needs to get screen data*/
 	winptr = NULL;
 }
 
+
 Nwindow::Nwindow(int len_y, int len_x, int x, int y){
     winptr = newwin(len_y, len_x, y, x);
-
     pos_y = 0; pos_x =0;
     MAX_Y = len_y; 
     MAX_X = len_x;
 }
 
 
-
 void Nwindow::print_color(ifstream& file,COLOR c){
-	
 	 char line[MAXCHAR]; int i = 0;
 	 while(!file.eof()){
-		 //if the max line is greater than term*
-		 //if (++i > MAX_Y) return;
 		 file.getline(line, MAXCHAR);
-		 //if ( file.gcount() > MAX_X) return;
+		 assert(i < MAX_Y && file.gcount() < MAX_X);
+		 ++i;
 		 print_line(string(line),c);
 	 }
 	 
@@ -35,35 +31,32 @@ void Nwindow::print_color(ifstream& file,COLOR c){
 void Nwindow::print_color(string str, COLOR c){
      set_color(c);
      mvwprintw(winptr,pos_y, pos_x,str.c_str());
-     //wprintw(winptr,"%s",str.c_str());
      wrefresh(winptr);
-     //reset_color();
+     reset_color(c);
 } 
-
-
 
 
 void Nwindow::print_color(double* da, COLOR color, int startp){
 
 }
 
+
 void Nwindow::clear(){
 	werase(winptr);
 }
 
+
 void Nwindow::print_line(string line, COLOR c){
-		 int aspace = get_end(line);
-		 int bspace = get_start(line);
+	int aspace = get_end(line);
+        int bspace = get_start(line);
 
-		 if ( bspace < 0 || aspace < 0 ) return;
-		 int prev_x = pos_x;
-		 move(pos_y,pos_x + bspace);
-		 assert(bspace != aspace);
-		 print_color(line.substr(bspace,line.length() - aspace),c);
-		 move(pos_y +1,prev_x);
-
-		
+	assert(bspace >= 0 &&  aspace >= 0);
+	int prev_x = pos_x;
+	move(pos_y,pos_x + bspace);
+	print_color(line.substr(bspace,line.length() - aspace),c);
+	move(pos_y +1,prev_x);
 }
+
 
 int Nwindow::get_end(string str){
 	auto it = str.rbegin();
@@ -74,6 +67,7 @@ int Nwindow::get_end(string str){
 	}
 	return -1;
 }
+
 
 int Nwindow::get_start(string str){
 	auto it = str.begin();
@@ -86,10 +80,12 @@ int Nwindow::get_start(string str){
 }
 
 
-void Nwindow::set_color(COLOR c){
-	//wattrset(winptr,COLOR_PAIR(1));
+void Nwindow::set_bkg_color(COLOR c){
     wbkgd(winptr,COLOR_PAIR(1));
+}
 
+void Nwindow::set_color(COLOR c){
+    wattrset(winptr,COLOR_PAIR(1));
 }
 
 
